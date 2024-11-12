55ee01e6d0f7509933ca281e02ebdcc0898f4931
shantanubr <mahaleshantanu3@gmail.com>
2024-11-12 19:50:38 +0530

    handling mobile view

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,11 +22,16 @@ export default function RootLayout({
       <head>
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
       </head>
-      <body className={inter.className + ' hidden sm:block'}>
-        <ThemeProvider>
-          <Header />
-          {children}
-        </ThemeProvider>
+      <body className={inter.className}>
+        <div className='hidden sm:block'>
+          <ThemeProvider>
+            <Header />
+            {children}
+          </ThemeProvider>
+        </div>
+        <div className='sm:hidden flex items-center justify-center h-screen font-bold bg-gradient-to-r from-indigo-700 via-purple-700 to-pink-700 dark:from-indigo-500 dark:via-purple-500 dark:to-pink-500 bg-clip-text text-transparent'>
+          {`Use Desktop For The Best Experience`}
+        </div>
       </body>
     </html>
   );
